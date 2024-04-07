881fbc001b4c741b6cc20ad3ea65358d627caf35
Ekin Ilter Varli <ekinnos@tutanota.com>
2024-04-07 17:09:28 +0300

    homepage updated

--- bilgiplus/bilgiplus/settings.py
+++ bilgiplus/bilgiplus/settings.py
@@ -42,7 +42,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'core'
+    'core',
+
+    # third-party
+    'compressor',
 ]
 
 MIDDLEWARE = [
@@ -122,6 +125,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = '/static/'
+COMPRESS_ROOT = BASE_DIR / 'static'
+
+COMPRESS_ENABLED = True
+
+STATICFILES_FINDERS = ('compressor.finders.CompressorFinder',)
+
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
